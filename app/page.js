1388112eb09d597447ddeb6ee030343e3f86352a
Thitikorn Sangissarapirak <ramenguykung@gmail.com>
2024-08-05 23:49:16 +0700

    Implement "SpeedInsights"

--- app/page.js
+++ app/page.js
@@ -1,5 +1,6 @@
 import Image from "next/image";
 import styles from "./page.module.css";
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function Home() {
   return (
