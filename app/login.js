1acbfb01744c1b5dd1387f9c255746a1a0d17066
Thitikorn Sangissarapirak <69292304+ramenguykung@users.noreply.github.com>
2024-08-09 11:32:29 +0000

    Modify login.js and CSS modules.

--- app/login.js
+++ app/login.js
@@ -1,7 +1,11 @@
+import image from 'next/image';
 import Link from 'next/link';
-import style form './page.module.css':
+import style from './page.module.css';
+import { SpeedInsights } from "@vercel/speed-insights/next";
 
 export default function login() {
 	return (
-	)
+    
+    <SpeedInsights/>
+	);
 }
