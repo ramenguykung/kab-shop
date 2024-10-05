827dd72674cfed0db981272d99f38c314ea251f2
Thitikorn Sangissarapirak <ramenguykung@gmail.com>
2024-10-06 00:18:02 +0700

    Implementing FirebaseSDK

---
+++ src/app/firebase/config.js
@@ -0,0 +1,18 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
+  authDomain: "kab-shop.firebaseapp.com",
+  databaseURL: "https://kab-shop-default-rtdb.asia-southeast1.firebasedatabase.app",
+  projectId: "kab-shop",
+  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
+  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
